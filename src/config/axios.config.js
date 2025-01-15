24fc7cfee3ea7d733cd0f1adc01592cb5a257f36
zubair arif <104222183+ZubairArif234@users.noreply.github.com>
2025-01-15 12:12:04 +0500

    Live URL

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // export const baseURL = "http://192.168.0.196:8001/";
-export const baseURL = "http://192.168.100.18:8001/";
-// export const baseURL = "https://mudeem-be-production.up.railway.app/";
+//export const baseURL = "http://192.168.100.18:8001/";
+export const baseURL = "https://mudeem-be-production.up.railway.app/";
 
 // axios instance for json data
 const custAxios = axios.create({
