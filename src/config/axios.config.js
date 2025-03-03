9261cc641a2d8397ee145a97f4c631157e0a1e6b
zubair arif <104222183+ZubairArif234@users.noreply.github.com>
2025-03-03 10:06:28 +0500

    Update axios.config.js

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const baseURL = "http://192.168.0.196:8001/";
-// export const baseURL = "https://mudeem-be-production.up.railway.app/";
+// export const baseURL = "http://192.168.0.196:8001/";
+export const baseURL = "https://mudeem-be-production.up.railway.app/";
 
 // axios instance for json data
 const custAxios = axios.create({
