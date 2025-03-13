0d33d76b16cac6ea6472160c2099f5e1112fdab8
umer-2001 <104222183+ZubairArif234@users.noreply.github.com>
2025-03-13 13:10:56 +0500

    ...,

--- src/adminPages/innovation/index.jsx
+++ src/adminPages/innovation/index.jsx
@@ -5,12 +5,11 @@ import InnovationCard from "../../components/custom/innovation/card";
 import { useGetProjects } from "../../hook/apis/sustainableInnovation/useGetProjects";
 import Loader from "../../components/custom/extra/loader";
 import DataNotFound from "../../components/custom/extra/dataNotFound";
-
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { toast } from "sonner";
 import { useChangeStatus } from "../../hook/apis/sustainableInnovation/useChangeStatus";
-import { SquarePagination } from "../../components/PaginationLayer";
+
 import { z } from "zod";
 
 const RewardSchema = z.object({
